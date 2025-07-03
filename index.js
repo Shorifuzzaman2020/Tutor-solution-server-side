3f6c077084b3718d24cd111810e430999f57dfc9
Shorifuzzaman2020 <shorifuzzamansoil2020@gmail.com>
2025-07-03 10:36:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.get('/',(req, res) =>{
+    res.send('Tutor server site')
+})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzmpelq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// Database connection and server startup
+async function startServer() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
+    
+    // Start Express server
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    process.exit(1);
+  }
+}
+
+startServer();
